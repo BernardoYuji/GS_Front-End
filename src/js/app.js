659873deb5dea7f2bc95e960560040a60b8bb2f5
arriatea <marcoas.arriate@gmail.com>
2025-06-06 17:30:45 -0300

    adição do slideshow + modificação menu hamburguer

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navList = document.querySelector('nav ul');
-
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.querySelector('.menu');
+  if (hamburger && menu) {
     hamburger.addEventListener('click', () => {
-        navList.classList.toggle('show');
+      menu.classList.toggle('show');
     });
-});
+  }
 
+  const slides = document.querySelectorAll('.slide');
+  if (slides.length > 0) {
+    iniciarSlideshow(slides);
+  }
+});
 
 function mudarCor(cor) {
   document.body.style.backgroundColor = cor;
+}
+
+function iniciarSlideshow(slides) {
+  let i = 0;
+  setInterval(() => {
+    slides[i].classList.remove('active');
+    i = (i + 1) % slides.length;
+    slides[i].classList.add('active');
+  }, 3000);
 }
